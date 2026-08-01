096378d8f7dbceba1f6e3988d6efc05b308d0844
lcostadev <lucas.ctsv@gmail.com>
2026-08-01 19:59:47 -0300

    update

--- Main/src/features/Feature04.java
+++ Main/src/features/Feature04.java
@@ -16,14 +16,13 @@ public class Feature04 {
         b.x = sc.nextDouble();
         b.y = sc.nextDouble();
         b.z = sc.nextDouble();
-        double pa = (a.x + a.y + a.z) / 2.0;
-        double pb = (b.x + b.y + b.z) / 2.0;
-        double areaA = Math.sqrt(pa * (pa - a.x) * (pa - a.y) * (pa - a.z));
-        double areaB = Math.sqrt(pb * (pb - b.x) * (pb - b.y) * (pb - b.z));
+        double areaA = a.area();
+        double areaB = b.area();
         System.out.printf("A area do triangulo A: %.4f\n", areaA);
         System.out.printf("A area do triangulo B: %.4f\n", areaB);
         if (areaA > areaB) System.out.println("A area do triangulo A é maior que a do triangulo B.");
         else if (areaA < areaB) System.out.println("A area do triangulo B é maior que a do triangulo A.");
-        else System.out.println("A area do triangulo B é igual a do triangulo A");
+        else if (areaA == areaB) System.out.println("A area do triangulo B é igual a do triangulo A");
+        else System.out.println("invalido!");
     }
 }
--- Main/src/features/entities/Triangle.java
+++ Main/src/features/entities/Triangle.java
@@ -4,4 +4,9 @@ public class Triangle {
     public double x;
     public double y;
     public double z;
+
+    public double area() {
+        double p = (x + y + z) / 2.0;
+        return Math.sqrt(p * (p - x) * (p - y) * (p - z));
+    }
 }
