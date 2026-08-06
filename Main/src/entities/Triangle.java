b41f0dade4f52f8c56f9971ed6289d6ca9e39844
lcostadev <lucas.ctsv@gmail.com>
2026-08-06 20:09:26 -0300

    update

--- Main/src/entities/Triangle.java
+++ Main/src/entities/Triangle.java
@@ -1,23 +1,37 @@
 package entities;
 
 public class Triangle {
-    public double x;
-    public double y;
-    public double z;
-
-    public Triangle() {
-    }
+    private double x;
+    private double y;
+    private double z;
 
     public Triangle(double x, double y, double z) {
+        validateTriangle(x, y, z);
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-
-
     public double area() {
         double p = (x + y + z) / 2.0;
         return Math.sqrt(p * (p - x) * (p - y) * (p - z));
+    }
+
+    private static void validateTriangle(double x, double y, double z) {
+        if (x <= 0 || y <= 0 || z <= 0 || (x + y <= z) || (x + z <= y) || (y + z <= x)) {
+            throw new IllegalArgumentException("The provided measurements do not form a valid triangle.");
+        }
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
     }
 }
